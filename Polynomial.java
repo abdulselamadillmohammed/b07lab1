3f3377dad565e1a0ba7da4780bc18dd29c7a67d4
abdulselamadillmohammed <forcoding39@gmail.com>
2025-09-20 11:38:54 -0400

    Add instruction dii

---
+++ Polynomial.java
@@ -0,0 +1,8 @@
+class Polynomial:
+    
+    // If you don't add visibility keyword, it defaults to package private
+    private double[] coefficients;
+
+    public Polynomial(){
+        coeffcients = new double[]{0};
+    }
